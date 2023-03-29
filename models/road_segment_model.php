ccdfe7de1fc1b38b9e99cee884bffd6ab966b716
Gideon Donkor Bonsu <gideon.bonsu@ashesi.edu.gh>
2023-03-29 13:17:55 +0000

    Added a definition of road segments

--- models/road_segment_model.php
+++ models/road_segment_model.php
@@ -7,11 +7,13 @@ namespace controllers;
         * CREATE OPERATION
         * Defines the INSERT querry to put the data from the android devices in the database.
         * @param accelerometer_datafile - data from android devices
+          "INSERT INTO `datafromphones`(`accelerometer_file`) 
+          VALUES ('$accelerometer_datafile')"
         **/
-        protected function insertRoadSegment($accelerometer_datafile){
+        protected function insertRoadSegment($latitiude, $longitude){
             $results = mysqli_query($this -> connect(),
-            "INSERT INTO `datafromphones`(`accelerometer_file`) 
-             VALUES ('$accelerometer_datafile')"
+            "INSERT INTO circles (id, circle) 
+            VALUES (ST_Buffer(POINT('$latitude','$longitude'), 5))"
            );
            return $results;
        }
@@ -30,3 +32,36 @@ namespace controllers;
 
     }
 ?>
+
+/*
+* -- phone accelerometer and GPS data
+CREATE TABLE phone_accelerometer_data (
+  data_id PRIMARY KEY AUTO_INCREMENT,
+  acceleration_zmean FLOAT,
+  acceleration_zstd FLOAT,
+  acceleration_zvariance FLOAT,
+  acceleration_zpeak FLOAT,
+  acceleration_zlow FLOAT,
+  latitude FLOAT,
+  longitude FLOAT,
+  time_recorded TIMESTAMP
+);
+
+-- create circular road segments
+CREATE TABLE road_segments (
+  road_segment_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  road_segment_circle GEOMETERY,
+  segment_roughness_index INTEGER,
+);
+ 
+-- road datapoints that make up a segment
+CREATE TABLE road_datapoints(
+  road_datapoint_id INTEGER PRIMARY KEY,
+  road_datapoint GEOMETERY,
+  roughness_index INTEGER,
+  road_segment_id INTEGER,
+  time_recorded TIMESTAMP,
+  FOREIGN KEY (road_segment_id) REFERENCES road_segments(road_segment_id)
+);
+*
+*/
